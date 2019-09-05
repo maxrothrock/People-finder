110194f7465aecffac80eb0b23cc3d394da86857
Max Rothrock <maxrothrock@gmail.com>
2019-09-05 16:26:40 -0700

    Initial Commit

---
+++ People finder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  People finder
+//
+//  Created by Max Rothrock on 9/5/19.
+//  Copyright © 2019 Max Rothrock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
